ecd70ef59c9ce9163853fa4db24a337f34b39eae
zerradi <dasileker@gmail.com>
2021-02-17 16:45:49 +0100

    add the display function

--- js/index.js
+++ js/index.js
@@ -1,21 +1,40 @@
 let myLibrary = [];
-
+// const { form } = document.forms
 const author = document.querySelector('#author');
 const title = document.querySelector('#title');
 const pages = document.querySelector('#pages');
 const read = document.querySelector('#read');
 const submit = document.querySelector('#submit');
 
-function Book(name, title, pages, read) {
-  this.name = name;
+function Book (author, title, pages, read) {
+  this.author = author;
   this.title = title;
   this.pages = pages;
   this.read = read;
 }
 
-function addBookToLibrary() {
-  
-  read.checked
+const addBookToLibrary = () => {
+  console.log('test');
+  const CurrentBook = new Book(author.value, title.value, pages.value, read.checked);
+  myLibrary.push(CurrentBook);
+  // console.log(myLibrary);
+  myLibrary.forEach(book => console.log(book.title))
+  return false;
 }
 
+
+// displayBook = () => {
+
+//   myLibrary.forEach(book => console.log(book.title))
+    
+//   const form = document.querySelector('#form');
+//   const input = document.createElement('input');
+
+//   title.textContent = book.title
+//   author.textContent = book.author
+
+  
+// }
+
+// console.log(displayBook);
 submit.onclick = addBookToLibrary;
